13c718ed9775837906df14e1da36656d54f077d8
Jonas Eberle <flightvision@googlemail.com>
2024-12-25 21:31:29 +0100

    raise version

--- otv/globals.py
+++ otv/globals.py
@@ -1,5 +1,5 @@
 AUTHOR = "Donovan C. Young"
 PROGRAM = "Ortho4XP Tile Validator"
 PROGRAM_SHORT = "otv"
-VERSION = "1.1.1"
+VERSION = "1.1.2-jonaseberle"
 VERSION_STR = "{} ({}) v{} - written by {}".format(PROGRAM, PROGRAM_SHORT, VERSION, AUTHOR)
